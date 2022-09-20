6d356c9c1796625d71295145ec3b238c53a6f165
meo-s <kmeos1579@gmail.com>
2022-09-20 22:40:14 +0900

    feat: add tcp traffic sender

--- scratch/udp_application/example-wifi.cc
+++ scratch/udp_application/example-wifi.cc
@@ -12,194 +12,255 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  * Author: Chang-Hui Kim <kch9001@gmail.com>
  */
 
-#include "ns3/core-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/network-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/wifi-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/random-variable-stream.h"
-#include "ns3/udp-client-server-helper.h"
-#include "fair-udp.h"
-#include "fair-udp-helper.h"
 #include "config.h"
-#include <string>
+#include "fair-udp-helper.h"
+#include "fair-udp.h"
+#include "ns3/application-container.h"
+#include "ns3/applications-module.h"
+#include "ns3/command-line.h"
+#include "ns3/core-module.h"
+#include "ns3/double.h"
+#include "ns3/inet-socket-address.h"
+#include "ns3/internet-module.h"
+#include "ns3/log.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/node-container.h"
+#include "ns3/nstime.h"
+#include "ns3/object.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/onoff-application.h"
+#include "ns3/packet-sink-helper.h"
+#include "ns3/packet.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/rng-seed-manager.h"
+#include "ns3/string.h"
+#include "ns3/udp-client-server-helper.h"
+#include "ns3/wifi-module.h"
 #include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <iterator>
+#include <string>
+#include <array>
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("example-wifi");
+NS_LOG_COMPONENT_DEFINE ("example-wifi");
 
-enum special_nodes
-  {
-    WIFI_AP = 0,
-    P2P_SERVER = 1,
-  };
+enum SpecialNodes {
+  WIFI_AP = 0,
+  P2P_SERVER = 1,
+};
 
 class DummyStream : public PacketSource
 {
 public:
-  DummyStream(std::string msg):
-    msg_(msg)
+  DummyStream (std::string msg) : msg_{msg}
   {
   }
 
-  Ptr<Packet>
-  GetPacket() override
-  {
-    auto packet = Create<Packet>(reinterpret_cast<uint8_t *>(msg_.data()), msg_.size());
-    return packet;
-  }
+  Ptr<Packet> GetPacket () override;
+
 private:
   std::string msg_;
 };
 
-void draw_please(FairUdpApp* client)
+Ptr<Packet> DummyStream::GetPacket ()
 {
-  static int id = 1;
-  client->Draw("node" + std::to_string(id));
-  id++;
+  return Create<Packet> (reinterpret_cast<uint8_t *> (msg_.data ()), msg_.size ());
 }
 
-int
-main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
+  using namespace std::string_literals;
+
+  auto SIMUL_TIME = TEST_TIME;
+  auto PROTOCOL = "udp"s;
+  auto SERVER_BANDWIDTH = "1000Mbps"s;
+  auto NUM_UAVS = UAV_NUM;
+  auto EXCLUSIVE_SENDER = false;
+
+  auto cmd = CommandLine{__FILE__};
+  cmd.AddValue ("protocol", "", PROTOCOL);
+  cmd.AddValue ("server-bandwidth", "", SERVER_BANDWIDTH);
+  cmd.AddValue ("uavs", "", NUM_UAVS);
+  cmd.AddValue ("simul_time", "", SIMUL_TIME);
+  cmd.AddValue ("exclusive_sender", "", EXCLUSIVE_SENDER);
+  cmd.Parse (argc, argv);
+
+  {
+    constexpr auto ALLOWED_PROTOCOLS = ::std::array{"udp", "fudp"};
+    if (::std::all_of (ALLOWED_PROTOCOLS.begin (), ALLOWED_PROTOCOLS.end (),
+                       [&PROTOCOL] (auto v) { return PROTOCOL != v; }))
+      {
+        ::std::exit (-1);
+      }
+  }
+
   // wired part
-  NodeContainer p2pNodes(2);
+  auto p2pNodes = NodeContainer{2};
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-  pointToPoint.SetChannelAttribute("Delay", StringValue("1ms"));
+  auto p2pHelper = PointToPointHelper{};
+  p2pHelper.SetDeviceAttribute ("DataRate", StringValue (SERVER_BANDWIDTH));
+  p2pHelper.SetChannelAttribute ("Delay", StringValue ("1ms"));
 
-  auto p2pDevices = pointToPoint.Install(p2pNodes);
+  auto p2pDevices = p2pHelper.Install (p2pNodes);
 
   // wifi part
-  NodeContainer wifiStaNodes(UAV_NUM);
-  auto wifiApNode = p2pNodes.Get(special_nodes::WIFI_AP);
+  auto wifiStaNodes = NodeContainer{NUM_UAVS * (EXCLUSIVE_SENDER ? 2 : 1)};
+  auto wifiApNode = p2pNodes.Get (SpecialNodes::WIFI_AP);
 
-  auto channel = YansWifiChannelHelper::Default();
-  auto phy = YansWifiPhyHelper();
-  phy.SetChannel(channel.Create());
+  auto channel = YansWifiChannelHelper::Default ();
+  auto phy = YansWifiPhyHelper ();
+  phy.SetChannel (channel.Create ());
 
   WifiMacHelper mac;
-  auto ssid = Ssid("ns-3-ssid");
+  auto ssid = Ssid ("ns-3-ssid");
 
   WifiHelper wifi;
-  wifi.SetStandard(WIFI_STANDARD_80211ac);
-  mac.SetType("ns3::StaWifiMac",
-              "Ssid", SsidValue (ssid),
-              "ActiveProbing", BooleanValue(false));
-  auto staDevices = wifi.Install(phy, mac, wifiStaNodes);
+  wifi.SetStandard (WIFI_STANDARD_80211ac);
+  mac.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (ssid), "ActiveProbing", BooleanValue (false));
+  auto staDevices = wifi.Install (phy, mac, wifiStaNodes);
 
-  mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
-  auto apDevices = wifi.Install(phy, mac, wifiApNode);
+  mac.SetType ("ns3::ApWifiMac", "Ssid", SsidValue (ssid));
+  auto apDevices = wifi.Install (phy, mac, wifiApNode);
 
   // mobility for stas
   MobilityHelper mobility;
-  mobility.SetPositionAllocator("ns3::UniformDiscPositionAllocator",
-                                "rho", DoubleValue(10.0),
-                                "X", DoubleValue(50),
-                                "Y", DoubleValue(50));
+  mobility.SetPositionAllocator ("ns3::UniformDiscPositionAllocator", "rho", DoubleValue (10.0), "X", DoubleValue (50),
+                                 "Y", DoubleValue (50));
 
-  mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel");
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel");
   // mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  mobility.Install(wifiStaNodes);
-
+  mobility.Install (wifiStaNodes);
 
   // mobility for ap
   MobilityHelper mobility_ap;
-  auto position_alloc = CreateObject<ListPositionAllocator>();
-  position_alloc->Add(Vector(50, 50, 0));
-  mobility_ap.SetPositionAllocator(position_alloc);
-  mobility_ap.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  mobility_ap.Install(wifiApNode);
+  auto position_alloc = CreateObject<ListPositionAllocator> ();
+  position_alloc->Add (Vector (50, 50, 0));
+  mobility_ap.SetPositionAllocator (position_alloc);
+  mobility_ap.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility_ap.Install (wifiApNode);
 
   // internet setting
   InternetStackHelper stack;
-  stack.Install(p2pNodes.Get(special_nodes::P2P_SERVER));
-  stack.Install(wifiApNode);
-  stack.Install(wifiStaNodes);
+  stack.Install (p2pNodes.Get (SpecialNodes::P2P_SERVER));
+  stack.Install (wifiApNode);
+  stack.Install (wifiStaNodes);
 
   Ipv4AddressHelper address;
 
-  address.SetBase("10.1.1.0", "255.255.255.0");
-  auto p2pInterfaces = address.Assign(p2pDevices);
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  auto p2pInterfaces = address.Assign (p2pDevices);
 
-  address.SetBase("10.1.3.0", "255.255.255.0");
-  address.Assign(staDevices);
-  address.Assign(apDevices);
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+  address.Assign (staDevices);
+  address.Assign (apDevices);
 
-  if constexpr (TARGET_PROTO == FAIR_UDP)
+  SeedManager::SetSeed (1423);
+  auto rng = CreateObject<UniformRandomVariable> ();
+
+  auto const serverIpv4 = p2pInterfaces.GetAddress (SpecialNodes::P2P_SERVER);
+  auto const serverPort = 7777;
+  auto const serverAddress = InetSocketAddress{serverIpv4, serverPort};
+
+  auto clientApps = ApplicationContainer{};
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Setup UDP clients and server
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  auto dummyData = DummyStream{::std::string (1024, '\0')};
+  if (PROTOCOL == "fudp")
     {
-      FairUdpHelper please(InetSocketAddress(p2pInterfaces.GetAddress(special_nodes::P2P_SERVER), 7777));
-      std::string msg;
-      msg.resize(1024);
-      DummyStream s(msg);
+      p2pNodes.Get (SpecialNodes::P2P_SERVER)->AddApplication (CreateObject<FairUdpApp> ());
+      LogComponentEnable ("FairUdpApp", LOG_LEVEL_INFO);
 
-      auto clients = please.Install(wifiStaNodes);
+      auto udpClientHelper = FairUdpHelper{serverAddress};
+      for (auto iter = wifiStaNodes.Begin (); iter != wifiStaNodes.End (); ++iter)
+        {
+          if (EXCLUSIVE_SENDER && NUM_UAVS <= ::std::distance (wifiStaNodes.Begin (), iter))
+            {
+              break;
+            }
 
-      SeedManager::SetSeed(1423);
-      std::for_each(clients.Begin(), clients.End(), [&s](auto client)
-      {
-        auto random_generator = CreateObject<UniformRandomVariable>();
-        auto jitter = random_generator->GetInteger(0, 1000);
-        client->SetStartTime(Seconds(0));
-        Simulator::Schedule(MilliSeconds(jitter), &FairUdpApp::SendStream, static_cast<FairUdpApp *>(&*client), &s);
-      });
+          auto udpClient = udpClientHelper.Install (*iter).Get (0);
+          udpClient->SetStartTime (Seconds (0));
+          clientApps.Add (udpClient);
 
-      clients.Stop(Seconds(TEST_TIME));
-
-      auto server = CreateObject<FairUdpApp>();
-      auto server_node = p2pNodes.Get(special_nodes::P2P_SERVER);
-      server_node->AddApplication(server);
-      LogComponentEnable("FairUdpApp", LOG_LEVEL_INFO);
-
-      // generate trace file
-      phy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
-      pointToPoint.EnablePcapAll("fair-udp");
-      phy.EnablePcap("fair-udp", apDevices.Get(special_nodes::WIFI_AP));
+          auto const scheduleDelay = MilliSeconds (rng->GetInteger (0, 1000));
+          auto udpClientRawPtr = static_cast<FairUdpApp *> (&(*udpClient));
+          Simulator::Schedule (scheduleDelay, &FairUdpApp::SendStream, udpClientRawPtr, &dummyData);
+        }
     }
-  else                          // udp for now
+  else // if (PROTOCOL == "udp")
     {
-      UdpServerHelper server(7777);
-      auto apps = server.Install(p2pNodes.Get(special_nodes::P2P_SERVER));
-      apps.Start(Seconds(0));
-      apps.Stop(Seconds(TEST_TIME));
+      auto udpServerApp = UdpServerHelper{serverPort}.Install (p2pNodes.Get (SpecialNodes::P2P_SERVER)).Get (0);
+      udpServerApp->SetStartTime (Seconds (0));
+      udpServerApp->SetStopTime (Seconds (SIMUL_TIME));
 
       uint32_t max_packet_size = 1024;
-      Time interval = MilliSeconds(1);
-      uint32_t max_packet_count = UINT32_MAX;
-      auto server_addr = p2pInterfaces.GetAddress(special_nodes::P2P_SERVER);
-      UdpClientHelper client(server_addr, 7777);
-      client.SetAttribute ("MaxPackets", UintegerValue (max_packet_count));
-      client.SetAttribute ("Interval", TimeValue (interval));
-      client.SetAttribute ("PacketSize", UintegerValue (max_packet_size));
-      apps = client.Install(wifiStaNodes);
-      apps.Start(Seconds(0));
-      apps.Stop(Seconds(TEST_TIME));
+      auto udpClientHelper = UdpClientHelper{serverIpv4, serverPort};
+      udpClientHelper.SetAttribute ("MaxPackets", UintegerValue (UINT32_MAX));
+      udpClientHelper.SetAttribute ("Interval", TimeValue (MilliSeconds (1)));
+      udpClientHelper.SetAttribute ("PacketSize", UintegerValue (max_packet_size));
 
-      // generate trace file
-      phy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
-      pointToPoint.EnablePcapAll("normal-udp");
-      phy.EnablePcap("normal-udp", apDevices.Get(special_nodes::WIFI_AP));
+      for (auto iter = wifiStaNodes.Begin (); iter != wifiStaNodes.End (); ++iter)
+        {
+          if (EXCLUSIVE_SENDER && NUM_UAVS <= ::std::distance (wifiStaNodes.Begin (), iter))
+            {
+              break;
+            }
+
+          auto udpClient = udpClientHelper.Install (*iter).Get (0);
+          udpClient->SetStartTime (Seconds (0));
+          clientApps.Add (udpClient);
+        }
     }
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Setup TCP clients and server
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+  auto tcpServerHelper = PacketSinkHelper{"ns3::TcpSocketFactory", serverAddress};
+  auto tcpServerApp = tcpServerHelper.Install (p2pNodes.Get (SpecialNodes::P2P_SERVER)).Get (0);
+  tcpServerApp->SetStartTime (Seconds (0));
+  tcpServerApp->SetStopTime (Seconds (SIMUL_TIME));
 
-  Simulator::Stop(Seconds(TEST_TIME));
-  Simulator::Run();
-  Simulator::Destroy();
+  // https://www.nsnam.org/doxygen/tcp-star-server_8cc_source.html
+  auto tcpClientHelper = OnOffHelper{"ns3::TcpSocketFactory", p2pInterfaces.GetAddress (SpecialNodes::P2P_SERVER)};
+  tcpClientHelper.SetAttribute ("Remote", AddressValue{serverAddress});
+  for (auto iter = wifiStaNodes.Begin (); iter != wifiStaNodes.End (); ++iter)
+    {
+      if (EXCLUSIVE_SENDER && ::std::distance (wifiStaNodes.Begin (), iter) < NUM_UAVS)
+        {
+          continue;
+        }
 
-  // std::for_each(clients.Begin(), clients.End(), [](auto client)
-  // {
-  //   draw_please(dynamic_cast<FairUdpApp *>(&*client));
-  // });
+      auto tcpClient = tcpClientHelper.Install (*iter).Get (0);
+      tcpClient->SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=5]"));
+      tcpClient->SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=5]"));
+      tcpClient->SetStartTime (Seconds (0));
+      clientApps.Add (tcpClient);
+    }
+
+  // generate trace file
+  phy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+  p2pHelper.EnablePcapAll (PROTOCOL);
+  phy.EnablePcap (PROTOCOL, apDevices.Get (SpecialNodes::WIFI_AP));
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  clientApps.Stop (Seconds (SIMUL_TIME));
+  Simulator::Stop (Seconds (SIMUL_TIME));
+  Simulator::Run ();
+  Simulator::Destroy ();
 
   return 0;
 }
