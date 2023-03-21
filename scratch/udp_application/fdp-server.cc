e88b483d9acb087225f20452793abc8414b61253
MadPlayer <kch9001@gmail.com>
2023-03-21 20:39:42 +0900

    update author

--- scratch/udp_application/fdp-server.cc
+++ scratch/udp_application/fdp-server.cc
@@ -15,9 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Amine Ismail <amine.ismail@sophia.inria.fr>
- *                      <amine.ismail@udcast.com>
- *
+ * Author: Chang-Hui Kim <kch9001@gmail.com>
  */
 #include "ns3/address.h"
 #include "ns3/address-utils.h"
